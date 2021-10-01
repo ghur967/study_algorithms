bde7b007724c0c4f04c4cdded50e7c72d31ce5dd
afterainjw <afterainljw@gmail.com>
2021-10-01 20:02:53 +0900

    3wekk

---
+++ language/python/week3/behind_zero.py
@@ -0,0 +1,14 @@
+def reorder(arr, n):
+    j = 0
+    for i in range(n):
+        if arr[i] != 0:
+            arr[j] = arr[i]
+        j += 1
+    for i in range(k, n):
+        arr[i] = 0
+
+
+arr = [6, 0, 8, 2, 3, 0, 4, 0, 1]
+n = len(arr)
+reorder(arr, n)
+print(arr)
